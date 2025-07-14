7be51890882a163cdbbe037e66eff6c5d71ce613
angelo <angelo.jimenez@edu-its-it>
2025-07-14 15:32:55 +0200

    Upload folder project ControlloTempo

---
+++ ControlloTempo/src/main/java/com/meteoApi/ControlloTempo/ControlloTempoApplication.java
@@ -0,0 +1,13 @@
+package com.meteoApi.ControlloTempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlloTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlloTempoApplication.class, args);
+	}
+
+}
---
+++ ControlloTempo/src/test/java/com/meteoApi/ControlloTempo/ControlloTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meteoApi.ControlloTempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlloTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
